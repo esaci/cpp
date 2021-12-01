07412d4165f99812c20657812afde757779e91e1
Elias SACI <esaci@student.42.fr>
2021-12-01 02:15:01 +0100

    auto

--- cpp2/ex03/bsp.cpp
+++ cpp2/ex03/bsp.cpp
@@ -2,14 +2,19 @@
 
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-	Fixed v1(b.getx() - a.getx());
-	Fixed v2(b.gety() - a.gety());
-	Fixed vv1(c.getx() - a.getx());
-	Fixed vv2(point.gety() - a.gety());
+	Fixed cay(c.gety() - a.gety()); //dy ca
+	Fixed cax(c.getx() - a.getx()); //dx ca
+	Fixed bay(b.gety() - a.gety()); //dx ba
+	Fixed pay(point.gety() - a.gety()); //dy pa
+	Fixed pax(point.getx() - a.getx()); //dy pa
+	Fixed bax(b.getx() - a.getx()); //dx ba
 
-	Fixed w1 ((a.getx() * v1 + vv2 * v2 - point.getx() * v1) / (vv1 * v2 - (b.getx() - a.getx()) * v1));
-	Fixed w2 ((vv2 - w1 * vv1) / v1);
-	if (w1.toFloat() >= 0 && w2.toFloat() >= 0 && (w1 + w2).toFloat() <= 1)
+	if (std::abs(cay.toFloat()) <= 0.1f)
+		return (bsp(a, c, b, point));
+	Fixed w1 ((a.getx() * cay + pay * cax - point.getx() * cay) / (bay * cax - bax * cay));
+	Fixed w2 ((pay - (w1 * bay)) / cay);
+
+	if (w1.toFloat() > 0 && w2.toFloat() > 0 && (w1 + w2).toFloat() <= 1)
 		return (true);
 	return (false);
 }
--- cpp2/ex03/main.cpp
+++ cpp2/ex03/main.cpp
@@ -6,12 +6,35 @@ int main( void ) {
 		Point a(Fixed(0.0f), Fixed(0.0f));
 		Point b(Fixed(0.0f), Fixed(7.0f));
 		Point c(Fixed(7.0f), Fixed(0.0f));
-		Point point(Fixed(-1.0f), Fixed(1.0f));
+		Point point(Fixed(0.0f), Fixed(3.0f));
 
 		std::cout << "Point is "; 
-		if (!bsp(a, b, c, point))
+		if (bsp(a, b, c, point) == false)
 			std::cout << "Not ";
-		std::cout << " in the triangle ! \na: " << a << " b : " << b << " c : " << c << std::endl;
+		std::cout << "in the triangle ! \na : " << a << "\nb : " << b << "\nc : " << c << std::endl;
+	}
+	{
+		Point a(Fixed(0.0f), Fixed(0.0f));
+		Point b(Fixed(7.0f), Fixed(7.0f));
+		Point c(Fixed(2.0f), Fixed(3.0f));
+		Point point(Fixed(3.0f), Fixed(3.1f));
+
+		std::cout << "Point is "; 
+		if (bsp(a, b, c, point) == false)
+			std::cout << "Not ";
+		std::cout << "in the triangle ! \na : " << a << "\nb : " << b << "\nc : " << c << std::endl;
+	}
+	{
+		Point a(Fixed(0.0f), Fixed(0.0f));
+		Point b(Fixed(7.0f), Fixed(7.0f));
+		Point c(Fixed(2.0f), Fixed(3.0f));
+		Point point(Fixed(4.0f), Fixed(5.0f));
+
+		std::cout << "Point is "; 
+		if (bsp(a, b, c, point) == false)
+			std::cout << "Not ";
+		std::cout << "in the triangle ! \na : " << a << "\nb : " << b << "\nc : " << c << std::endl;
+		std::cout << "Point : " << point << std::endl;
 	}
 return (0);
 }
